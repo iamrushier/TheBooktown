ad31e05debfa30b5f86cb300fb7ce7ec1b6acefb
iamrushier <rushikeshsurve193@gmail.com>
2023-10-01 23:46:44 +0530

    Removed top header from home.php

--- header.php
+++ header.php
@@ -13,17 +13,17 @@ if(isset($message)){
 
 <header class="header">
 
-   <div class="header-1">
+   <!--div class="header-1">
       <div class="flex">
          <div class="share">
-            <a href="#" class="fab fa-facebook-f"></a>
-            <a href="#" class="fab fa-twitter"></a>
-            <a href="#" class="fab fa-instagram"></a>
-            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-facebook-f">facebook</a>
+            <a href="#" class="fab fa-twitter">twitter</a>
+            <a href="#" class="fab fa-instagram">instagram</a>
+            <a href="#" class="fab fa-linkedin">linkedin</a>
          </div>
          <p><a href="login.php">Login</a> | <a href="register.php">Register</a> </p>
       </div>
-   </div>
+   </div-->
 
    <div class="header-2">
       <div class="flex">
@@ -38,9 +38,9 @@ if(isset($message)){
          </nav>
 
          <div class="icons">
-            <div id="menu-btn" class="fas fa-bars"></div>
-            <a href="search_page.php" class="fas fa-search"></a>
-            <div id="user-btn" class="fas fa-user"></div>
+            <div id="menu-btn" class="fas fa-bars">Menu</div>
+            <a href="search_page.php" class="fas fa-search">Search</a>
+            <div id="user-btn" class="fas fa-user">User</div>
             <?php
                $select_cart_number = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
                $cart_rows_number = mysqli_num_rows($select_cart_number); 
@@ -49,9 +49,9 @@ if(isset($message)){
          </div>
 
          <div class="user-box">
-            <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
-            <p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
-            <a href="logout.php" class="delete-btn">logout</a>
+            <p>Username : <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>Email : <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <a href="logout.php" class="delete-btn">Log Out</a>
          </div>
       </div>
    </div>
